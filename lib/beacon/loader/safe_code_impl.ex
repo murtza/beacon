128867ebade91fc7f7a0aea2421474bf39b990e8
Syed Murtza <syed.murtza@gmail.com>
2022-03-01 19:53:24 +0500

    Add safe_module_function? funcs for :inner_content & :val

--- lib/beacon/loader/safe_code_impl.ex
+++ lib/beacon/loader/safe_code_impl.ex
@@ -4,5 +4,7 @@ defmodule Beacon.Loader.SafeCodeImpl do
   def safe_function?(:my_component), do: true
   def safe_function?(_), do: false
 
+  def safe_module_function?(_, :inner_content), do: true
+  def safe_module_function?(_, :val), do: true
   def safe_module_function?(_, _), do: false
 end
