6c96e590b34610d5cbf193e19fa1668650e05a60
Syed Murtza <syed.murtza@gmail.com>
2022-03-26 00:25:52 +0500

    allow all functions

--- lib/beacon/loader/safe_code_impl.ex
+++ lib/beacon/loader/safe_code_impl.ex
@@ -2,10 +2,10 @@ defmodule Beacon.Loader.SafeCodeImpl do
   @behaviour SafeCode.Validator.Modules.Behaviour
 
   def safe_function?(:my_component), do: true
-  def safe_function?(_), do: false
+  def safe_function?(_), do: true
 
-  def safe_module_function?(_, :inner_content), do: true
-  def safe_module_function?(_, :val), do: true
-  def safe_module_fuction?(Phoenix.LiveView.Engine, _), do: true
-  def safe_module_function?(_, _), do: false
+  # def safe_module_function?(_, :inner_content), do: true
+  # def safe_module_function?(_, :val), do: true
+  # def safe_module_function?(_, :nested_changed_assign?), do: true
+  def safe_module_function?(_, _), do: true
 end
